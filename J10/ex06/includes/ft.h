bf092ad83017720ccaca607db972af8e84282b22
Arthur GIULIANO <agiulian@e1r2p14.42.fr>
2016-07-19 09:54:46 +0200

    day 10

---
+++ J10/ex06/includes/ft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agiulian <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/16 19:05:04 by agiulian          #+#    #+#             */
+/*   Updated: 2016/07/18 22:09:59 by agiulian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _FT_H
+# define _FT_H
+
+void		ft_putchar(char c);
+void		ft_putstr(char *str);
+void		ft_putnbr(int nb);
+int			ft_atoi(char *str);
+void		ft_sub(int a, int b);
+void		ft_add(int a, int b);
+void		ft_div(int a, int b);
+void		ft_mod(int a, int b);
+void		ft_mul(int a, int b);
+
+#endif
